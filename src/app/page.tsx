c0d9fcffe11f9f188fc0a12f23cc8cb14591e12e
Vishal Kashi <dev.vishalkashi@gmail.com>
2025-02-28 13:14:10 +0530

    feat: add promotion section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Icon from "@/components/common/icon";
 import IconCard from "@/components/common/icon-card";
 import InfoPanel from "@/components/common/info-panel";
 import TestimonialCard from "@/components/common/testimonial-card";
@@ -234,7 +235,7 @@ export default function Home() {
           </p>
           <Button className="mt-8">Register to Explore Trampoline</Button>
         </div>
-        <div className="grid grid-cols-2 gap-8 w-[100%]">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-[100%]">
           <div className="p-4 border-l-4 rounded-xl border-primary-accent">
             <h3>Worried About Quality?</h3>
             <p>
@@ -269,6 +270,46 @@ export default function Home() {
       </div>
     );
   };
+  const promotionSection = () => {
+    return (
+      <section className="flex flex-col sm:flex-row gap-14 mt-16 p-8">
+        <div>
+          <h2>
+            Ready to Transform Your Inventory? Take the Leap With Zero Risk.
+          </h2>
+          <div className="flex flex-col gap-8 mt-16 mb-12">
+            <div className="flex gap-4 items-start">
+              <Icon src="/icons/check_square.svg" width={24} height={24} />
+              <p>
+                As a first-time buyer, enjoy £100 off your first order (Above
+                £500). Get the flexibility you need to test our quality and see
+                your customersʼ reactions.
+              </p>
+            </div>
+            <div className="flex gap-4 items-start">
+              <Icon src="/icons/check_square.svg" width={24} height={24} />
+              <p>
+                No Minimum Order Value on Your First Purchase: Test the waters
+                without committing to large shipments.
+              </p>
+            </div>
+            <div className="flex gap-4 items-start">
+              <Icon src="/icons/check_square.svg" width={24} height={24} />
+              <p>Flexible 30-Day Payment Terms on all initial orders.</p>
+            </div>
+          </div>
+          <Button>Claim My £100 Off & Register Now</Button>
+        </div>
+        <Image
+          src="/images/image_3.png"
+          width={722}
+          height={408}
+          alt=""
+          className="sm:w-[50%]"
+        />
+      </section>
+    );
+  };
   return (
     <section className="max-w-[1200px] mx-auto">
       {heroSection()}
@@ -276,6 +317,7 @@ export default function Home() {
       {featuresSection()}
       {testimonials()}
       {reassuranceSection()}
+      {promotionSection()}
     </section>
   );
 }
